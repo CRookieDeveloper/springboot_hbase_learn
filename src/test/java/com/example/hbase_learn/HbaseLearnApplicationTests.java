09aed14ad34afcdb14bcbff42670b94a92d2a991
RookieFpp <cfpp1995@gmail.com>
2022-08-20 23:22:19 +0800

    #commit 将本地创建的一个springboot项目上传

---
+++ src/test/java/com/example/hbase_learn/HbaseLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hbase_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbaseLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
